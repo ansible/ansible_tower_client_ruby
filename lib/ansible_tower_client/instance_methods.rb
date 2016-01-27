976bacbbc1396e64ecf9ed828162d05eb1e37de1
Drew Bomhof <dbomhof@redhat.com>
2016-01-27 15:43:44 -0500

    Line up equal signs

--- lib/ansible_tower_client/instance_methods.rb
+++ lib/ansible_tower_client/instance_methods.rb
@@ -3,9 +3,9 @@ module AnsibleTowerClient
     attr_reader :id, :name, :url, :raw_body
 
     def initialize(raw_body)
-      @id   = raw_body["id"]
-      @name = raw_body["name"]
-      @url = raw_body["url"]
+      @id       = raw_body["id"]
+      @name     = raw_body["name"]
+      @url      = raw_body["url"]
       @raw_body = raw_body
     end
 
