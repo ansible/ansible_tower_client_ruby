0a40a3d7ec3eaae63fd91e8b22933a317555e59c
Brandon Dunne <bdunne@redhat.com>
2016-02-11 18:09:17 -0500

    Expose Host.groups and Group.children (other groups) relationships

--- lib/ansible_tower_client/group.rb
+++ lib/ansible_tower_client/group.rb
@@ -6,5 +6,9 @@ module AnsibleTowerClient
     def self.endpoint
       "groups".freeze
     end
+
+    def children
+      self.class.collection_for(Api.get(File.join(self.class.endpoint, id.to_s, "children")))
+    end
   end
 end
--- lib/ansible_tower_client/host.rb
+++ lib/ansible_tower_client/host.rb
@@ -13,5 +13,9 @@ module AnsibleTowerClient
     def self.endpoint
       "hosts".freeze
     end
+
+    def groups
+      self.class.collection_for(Api.get(File.join(self.class.endpoint, id.to_s, "groups")))
+    end
   end
 end
