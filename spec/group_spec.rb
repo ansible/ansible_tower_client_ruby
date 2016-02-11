9d58c073bb7ebfc223dcec76911f057edca010cf
Brandon Dunne <bdunne@redhat.com>
2016-02-11 11:05:12 -0500

    Create shared examples for CollectionMethods

--- spec/group_spec.rb
+++ spec/group_spec.rb
@@ -5,28 +5,7 @@ describe AnsibleTowerClient::Group do
   let(:collection)     { build(:response_collection, :klass => described_class) }
   let(:instance)       { build(:response_instance,   :klass => described_class) }
 
-  context "Collection Methods" do
-    describe '.all' do
-      let(:get) { instance_double("Faraday::Result", :body => collection.to_json) }
-
-      it "returns a collection of objects" do
-        AnsibleTowerClient::Api.instance_variable_set(:@instance, api_connection)
-        obj_collection = described_class.all
-        expect(obj_collection).to        be_a Array
-        expect(obj_collection.length).to eq(2)
-        expect(obj_collection.first).to  be_a described_class
-      end
-    end
-
-    describe '.find' do
-      let(:get) { instance_double("Faraday::Result", :body => instance.to_json) }
-
-      it 'returns an instance' do
-        AnsibleTowerClient::Api.instance_variable_set(:@instance, api_connection)
-        expect(described_class.find(1)).to be_a described_class
-      end
-    end
-  end
+  include_examples "Collection Methods"
 
   it "#initialize instantiates an #{described_class} from a hash" do
     obj = described_class.new(instance)
