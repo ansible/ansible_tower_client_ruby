1a043e13a61a9cbdc9fe85279a36153f8afa7bf1
Brandon Dunne <bdunne@redhat.com>
2016-02-12 07:36:01 -0500

    Create response traits for host, group, and job_template

--- spec/group_spec.rb
+++ spec/group_spec.rb
@@ -3,15 +3,16 @@ require_relative 'spec_helper'
 describe AnsibleTowerClient::Group do
   let(:api_connection) { instance_double("Faraday::Connection", :get => get) }
   let(:collection)     { build(:response_collection, :klass => described_class) }
-  let(:instance)       { build(:response_instance,   :klass => described_class) }
+  let(:instance)       { build(:response_instance, :group, :klass => described_class) }
 
   include_examples "Collection Methods"
 
   it "#initialize instantiates an #{described_class} from a hash" do
     obj = described_class.new(instance)
 
-    expect(obj).to      be_a described_class
-    expect(obj.id).to   be_a Integer
-    expect(obj.name).to be_a String
+    expect(obj).to              be_a described_class
+    expect(obj.id).to           be_a Integer
+    expect(obj.inventory_id).to be_a Integer
+    expect(obj.name).to         be_a String
   end
 end
--- spec/host_spec.rb
+++ spec/host_spec.rb
@@ -4,17 +4,18 @@ require 'json'
 describe AnsibleTowerClient::Host do
   let(:api_connection) { instance_double("Faraday::Connection", :get => get) }
   let(:collection)     { build(:response_collection, :klass => described_class) }
-  let(:instance)       { build(:response_instance, :instance_id, :klass => described_class) }
+  let(:instance)       { build(:response_instance, :host, :klass => described_class) }
 
   include_examples "Collection Methods"
 
   it "#initialize instantiates an #{described_class} from a hash" do
     obj = described_class.new(instance)
 
-    expect(obj).to             be_a described_class
-    expect(obj.id).to          be_a Integer
-    expect(obj.url).to         be_a String
-    expect(obj.instance_id).to be_a String
-    expect(obj.name).to        be_a String
+    expect(obj).to              be_a described_class
+    expect(obj.id).to           be_a Integer
+    expect(obj.url).to          be_a String
+    expect(obj.instance_id).to  be_a String
+    expect(obj.inventory_id).to be_a Integer
+    expect(obj.name).to         be_a String
   end
 end
