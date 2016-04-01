2c74cae8b3e01d1751930aac6ba81a2557e7f22f
Drew Bomhof <dbomhof@redhat.com>
2016-04-01 12:41:37 -0400

    Add RaiseTowerError as a faraday response middleware

---
+++ lib/ansible_tower_client/middleware/raise_tower_error.rb
@@ -0,0 +1,23 @@
+module AnsibleTowerClient
+  module Middleware
+    class RaiseTowerError < Faraday::Response::Middleware
+      ClientErrorStatuses = 400...600
+
+      def on_complete(env)
+        case env[:status]
+        when 404
+          raise Faraday::Error::ResourceNotFound, response_values(env)
+        when 407
+          # mimic the behavior that we get with proxy requests with HTTPS
+          raise Faraday::Error::ConnectionFailed, %{407 "Proxy Authentication Required "}
+        when ClientErrorStatuses
+          raise Faraday::Error::ClientError, env.body
+        end
+      end
+
+      def response_values(env)
+        {:status => env.status, :headers => env.response_headers, :body => env.body}
+      end
+    end
+  end
+end
---
+++ spec/middleware/raise_tower_error_spec.rb
@@ -0,0 +1,27 @@
+require 'faraday'
+require 'ansible_tower_client/middleware/raise_tower_error'
+require_relative '../spec_helper'
+
+describe AnsibleTowerClient::Middleware::RaiseTowerError do
+  context "Faraday::Error" do
+    let(:env_400) { MockEnv.new('missing these attributes', 400) }
+    let(:env_404) { MockEnv.new('missing these attributes', 404) }
+    let(:env_407) { MockEnv.new('missing these attributes', 407) }
+
+    let(:error) { AnsibleTowerClient::Middleware::RaiseTowerError.new }
+
+    it "raises ClientError and returns the body message with a status 404" do
+      expect { error.on_complete(env_400) }.to raise_error(Faraday::Error::ClientError, "missing these attributes")
+    end
+
+    it "raises ResourceNotFound exception with a status 404" do
+      expect { error.on_complete(env_404) }.to raise_error(Faraday::Error::ResourceNotFound)
+    end
+
+    it "raises ConnectionFailed exception with a status 407" do
+      expect { error.on_complete(env_407) }.to raise_error(Faraday::Error::ConnectionFailed)
+    end
+  end
+end
+
+class MockEnv < Struct.new(:body, :status, :response_headers); end
