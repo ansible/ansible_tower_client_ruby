e0d280ed5a460ee70d63765aaf11858324a7324d
Drew Bomhof <dbomhof@redhat.com>
2016-04-07 16:41:41 -0400

    Specs to validate hashifying and dehashifying back to json

--- spec/support/shared_examples/collection_methods.rb
+++ spec/support/shared_examples/collection_methods.rb
@@ -9,8 +9,14 @@ shared_examples_for "Collection Methods" do
   end
 
   it ".find returns an instance" do
-    expect(api).to receive(:get).and_return(instance_double("Faraday::Result", :body => raw_instance.to_json))
+    expect(api).to receive(:get).at_least(:once).and_return(instance_double("Faraday::Result", :body => raw_instance.to_json))
 
     expect(collection.find(1)).to be_a described_class
+    attrs = described_class.const_get('EXCLUDED')
+    if attrs.present?
+      hashed_attribute = collection.find(1)[attrs.first]
+      expect(hashed_attribute.present?).to be true
+      expect(hashed_attribute).to be_a Hash
+    end
   end
 end
