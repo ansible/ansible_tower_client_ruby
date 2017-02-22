4f0cd8e769bac658a3d8e1c3a51a522adaeb16d0
Brandon Dunne <bdunne@redhat.com>
2017-02-22 18:06:16 -0500

    Fix error logging after changing error response classes
    Log the error from the middleware to avoid rerescuing AnsibleTowerClient
    errors in Api class.
    
    This will log:
    [----] D, [2017-02-22T17:47:41.987732 #17232:c27108] DEBUG -- : AnsibleTowerClient::Middleware::RaiseTowerError Response:
     method=:post,
     body=
      "{\"inventory\":[\"Job Template 'inventory' is missing or undefined.\"]}",
     url=#<URI::HTTPS https://10.x.x.x/api/v1/job_templates/7/launch/>,
     request=
      #<struct Faraday::RequestOptions
       params_encoder=nil,
       proxy=nil,
       bind=nil,
       timeout=nil,
       open_timeout=nil,
       boundary=nil,
       oauth=nil>,
     request_headers=
      {"Authorization"=>"Basic YWRtaW46cGFzc3dvcmQxMjM=",
       "User-Agent"=>"Faraday v0.9.2",
       "Content-Type"=>"application/json"},
     ssl=
      #<struct Faraday::SSLOptions
       verify=false,
       ca_file=nil,
       ca_path=nil,
       verify_mode=nil,
       cert_store=nil,
       client_cert=nil,
       client_key=nil,
       certificate=nil,
       private_key=nil,
       verify_depth=nil,
       version=nil>,
     parallel_manager=nil,
     params=nil,
     response=
      #<Faraday::Response:0x0000000ff38928
       @env=#<struct Faraday::Env:...>,
       @on_complete_callbacks=[]>,
     response_headers=
      {"date"=>"Wed, 22 Feb 2017 22:47:39 GMT",
       "server"=>
        "Apache/2.4.6 (Red Hat Enterprise Linux) OpenSSL/1.0.1e-fips mod_wsgi/3.4 Python/2.7.5",
       "vary"=>"Accept,Cookie",
       "allow"=>"GET, POST, HEAD, OPTIONS",
       "x-api-time"=>"0.115s",
       "content-length"=>"67",
       "connection"=>"close",
       "content-type"=>"application/json"},
     status=400>
    [----] E, [2017-02-22T17:47:41.988213 #17232:c27108] ERROR -- : AnsibleTowerClient::Middleware::RaiseTowerError Response Body:
    {"inventory"=>["Job Template 'inventory' is missing or undefined."]}

--- lib/ansible_tower_client/middleware/raise_tower_error.rb
+++ lib/ansible_tower_client/middleware/raise_tower_error.rb
@@ -1,9 +1,16 @@
 module AnsibleTowerClient
   module Middleware
     class RaiseTowerError < Faraday::Response::Middleware
+      include Logging
       CLIENT_ERROR_STATUSES = 400...600
 
       def on_complete(env)
+        return unless CLIENT_ERROR_STATUSES.include?(env[:status])
+        logger.debug { "#{self.class.name} Raw Response:\n#{env.pretty_inspect}" }
+        message   = JSON.parse(env.body).pretty_inspect rescue nil
+        message ||= env.body
+        logger.error("#{self.class.name} Response Body:\n#{message}")
+
         case env[:status]
         when 402
           raise AnsibleTowerClient::UnlicensedFeatureError
@@ -12,7 +19,7 @@ module AnsibleTowerClient
         when 407
           # mimic the behavior that we get with proxy requests with HTTPS
           raise AnsibleTowerClient::ConnectionError, %(407 "Proxy Authentication Required ")
-        when CLIENT_ERROR_STATUSES
+        else
           raise AnsibleTowerClient::ClientError, env.body
         end
       end
--- spec/middleware/raise_tower_error_spec.rb
+++ spec/middleware/raise_tower_error_spec.rb
@@ -12,19 +12,49 @@ describe AnsibleTowerClient::Middleware::RaiseTowerError do
 
     let(:error) { AnsibleTowerClient::Middleware::RaiseTowerError.new }
 
+    around do |example|
+      AnsibleTowerClient.logger.level = 0
+      example.run
+      AnsibleTowerClient.logger.level = 1
+    end
+
     it "raises ClientError and returns the body message with a status 400" do
+      expect(AnsibleTowerClient.logger).to receive(:debug) do |&block|
+        expect(block.call).to eq("#{described_class.name} Raw Response:\n#{env_400.pretty_inspect}")
+      end
+      expect(AnsibleTowerClient.logger).to receive(:error).with(
+        "AnsibleTowerClient::Middleware::RaiseTowerError Response Body:\nmissing these attributes"
+      )
       expect { error.on_complete(env_400) }.to raise_error(AnsibleTowerClient::ClientError, "missing these attributes")
     end
 
     it "raises UnlicensedFeatureError with a status 402" do
+      expect(AnsibleTowerClient.logger).to receive(:debug) do |&block|
+        expect(block.call).to eq("#{described_class.name} Raw Response:\n#{env_402.pretty_inspect}")
+      end
+      expect(AnsibleTowerClient.logger).to receive(:error).with(
+        "AnsibleTowerClient::Middleware::RaiseTowerError Response Body:\nmissing these attributes"
+      )
       expect { error.on_complete(env_402) }.to raise_error(AnsibleTowerClient::UnlicensedFeatureError)
     end
 
     it "raises ResourceNotFound exception with a status 404" do
+      expect(AnsibleTowerClient.logger).to receive(:debug) do |&block|
+        expect(block.call).to eq("#{described_class.name} Raw Response:\n#{env_404.pretty_inspect}")
+      end
+      expect(AnsibleTowerClient.logger).to receive(:error).with(
+        "AnsibleTowerClient::Middleware::RaiseTowerError Response Body:\nmissing these attributes"
+      )
       expect { error.on_complete(env_404) }.to raise_error(AnsibleTowerClient::ResourceNotFoundError)
     end
 
     it "raises ConnectionFailed exception with a status 407" do
+      expect(AnsibleTowerClient.logger).to receive(:debug) do |&block|
+        expect(block.call).to eq("#{described_class.name} Raw Response:\n#{env_407.pretty_inspect}")
+      end
+      expect(AnsibleTowerClient.logger).to receive(:error).with(
+        "AnsibleTowerClient::Middleware::RaiseTowerError Response Body:\nmissing these attributes"
+      )
       expect { error.on_complete(env_407) }.to raise_error(AnsibleTowerClient::ConnectionError)
     end
   end
