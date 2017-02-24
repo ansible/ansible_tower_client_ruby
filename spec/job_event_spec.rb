01285c189fb5e89a34355663984e2c25aaa28b11
Drew Bomhof <dbomhof@redhat.com>
2017-02-24 17:39:22 -0500

    Added JobEvent spec
    https://www.pivotaltracker.com/story/show/140601365

---
+++ spec/job_event_spec.rb
@@ -0,0 +1,10 @@
+describe AnsibleTowerClient::JobEvent do
+  let(:url)                 { "example.com/api/v1/job_events" }
+  let(:api)                 { AnsibleTowerClient::Api.new(instance_double("Faraday::Connection")) }
+  let(:collection)          { api.job_events }
+  let(:raw_url_collection)  { build(:response_url_collection, :klass => described_class, :url => url) }
+  let(:raw_instance)        { build(:response_instance, :group, :klass => described_class) }
+
+  include_examples "Collection Methods"
+  include_examples "Api Methods"
+end
