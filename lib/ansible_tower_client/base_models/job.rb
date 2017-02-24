b5d9a763416a3ed6c37f106f5dd5fc8752c6f798
Drew Bomhof <dbomhof@redhat.com>
2017-02-24 17:12:56 -0500

    Add job_plays as a Collection off of Job
    https://www.pivotaltracker.com/story/show/140561141

--- lib/ansible_tower_client/base_models/job.rb
+++ lib/ansible_tower_client/base_models/job.rb
@@ -4,6 +4,10 @@ module AnsibleTowerClient
       extra_vars.empty? ? {} : hashify(:extra_vars)
     end
 
+    def job_plays
+      Collection.new(api).find_all_by_url(related["job_plays"])
+    end
+
     def stdout
       out_url = related['stdout']
       return unless out_url
