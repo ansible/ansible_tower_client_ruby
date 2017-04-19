46770b6bf8478bd438f65e7ff457fc347d21cb15
Brandon Dunne <bdunne@redhat.com>
2017-04-19 14:52:52 -0400

    Move Collection#all & #find tests to collection_spec.rb

--- spec/collection_spec.rb
+++ spec/collection_spec.rb
@@ -1,4 +1,20 @@
 describe AnsibleTowerClient::Collection do
+  COLLECTION_CLASSES = [
+    AnsibleTowerClient::AdHocCommand,
+    AnsibleTowerClient::Credential,
+    AnsibleTowerClient::Group,
+    AnsibleTowerClient::Host,
+    AnsibleTowerClient::Inventory,
+    AnsibleTowerClient::InventorySource,
+    AnsibleTowerClient::InventoryUpdate,
+    AnsibleTowerClient::Job,
+    AnsibleTowerClient::JobEvent,
+    AnsibleTowerClient::JobTemplate,
+    AnsibleTowerClient::Organization,
+    AnsibleTowerClient::Project,
+    AnsibleTowerClient::ProjectUpdate,
+  ].freeze
+
   let(:connection)  { double("connection") }
   let(:mock_api)    { AnsibleTowerClient::Api.new(connection) }
   let(:instance)    { described_class.new(mock_api) }
@@ -6,6 +22,36 @@ describe AnsibleTowerClient::Collection do
   let(:get_options) { {"key" => "value"} }
   let(:response_1)  { double("response", :body => body_1) }
   let(:body_1)      { {"next_page" => nil, "results" => (1..5).collect { |i| {:id => i, :type => "host"} }}.to_json }
+
+  context "#all returns an enumerator that will delay load a collection" do
+    COLLECTION_CLASSES.each do |klass|
+      it "for #{klass}" do
+        klass_name = klass.name.split("::").last
+        collection_class = klass_name.underscore.pluralize
+
+        allow(mock_api).to receive(:config).and_return({}) # For Job Template class
+        collection = described_class.new(mock_api, klass)
+        expect(described_class).to receive(:new).with(mock_api, klass).and_return(collection)
+        expect(collection).to receive(:find_all_by_url).with(klass.endpoint, any_args)
+
+        mock_api.send(collection_class).all
+      end
+    end
+  end
+
+  context "#find" do
+    COLLECTION_CLASSES.each do |klass|
+      it "for #{klass}" do
+        klass_name = klass.name.split("::").last
+        collection_class = klass_name.underscore.pluralize
+
+        allow(mock_api).to receive(:config).and_return({}) # For Job Template class
+        expect(mock_api).to receive(:get).and_return(instance_double("Faraday::Result", :body => {}.to_json))
+
+        expect(mock_api.send(collection_class).find(1)).to be_instance_of(klass)
+      end
+    end
+  end
 
   context "#find_all_by_url" do
     it "is an Enumerator" do
--- spec/support/shared_examples/collection_methods.rb
+++ spec/support/shared_examples/collection_methods.rb
@@ -1,10 +1,4 @@
 shared_examples_for "Collection Methods" do
-  it ".all returns an enumerator that will delay load a collection" do
-    expect(collection).to receive(:find_all_by_url).with(described_class.endpoint, any_args)
-
-    collection.all
-  end
-
   it ".find_all_by_url returns a collection of objects via a url" do
     expect(api).to receive(:get).and_return(instance_double("Faraday::Result", :body => raw_url_collection.to_json))
     url = raw_url_collection['url']
@@ -30,11 +24,5 @@ shared_examples_for "Collection Methods" do
     expect(obj_collection_array.length).to    eq(8)
     expect(obj_collection_array.first).to_not eq described_class
     expect(obj_collection_array.first).to be_a String
-  end
-
-  it ".find returns an instance" do
-    expect(api).to receive(:get).and_return(instance_double("Faraday::Result", :body => raw_instance.to_json))
-
-    expect(collection.find(1)).to be_a described_class
   end
 end
