3d5fa66fe0739608e5224fbeac06c6cbfbe89354
Brandon Dunne <bdunne@redhat.com>
2017-04-19 13:31:38 -0400

    Add MockApi::AdHocCommand and begin to remove stubbing

--- spec/support/mock_api.rb
+++ spec/support/mock_api.rb
@@ -7,9 +7,11 @@ module AnsibleTowerClient
       end
     end
 
-    def get(path)
+    def get(path, get_options = nil)
       suffix = path.split("api/v1/").last
       case suffix
+      when "ad_hoc_commands"
+        wrap_response(AdHocCommand.response)
       when "config"
         wrap_response(Config.response)
       when "me"
