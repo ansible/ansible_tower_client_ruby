ee7615743cff65963ee5dbb520ee726bb9ee21a7
Marcel Hild <hild@b4mad.net>
2017-04-24 21:09:13 +0200

    translate organization_id to organization

--- spec/credential_spec.rb
+++ spec/credential_spec.rb
@@ -18,4 +18,16 @@ describe AnsibleTowerClient::Credential do
     expect(obj.username).to     be_a String
     expect(obj.name).to         be_a String
   end
+
+  context 'override_raw_attributes' do
+    let(:obj) { described_class.new(instance_double("Faraday::Connection"), raw_instance) }
+    let(:instance_api) { obj.instance_variable_get(:@api) }
+
+    it 'translates :organization to :organization_id for update_attributes' do
+      raw_instance[:organization_id] = 10
+      expect(instance_api).to receive(:patch).and_return(instance_double("Faraday::Result", :body => raw_instance.to_json))
+      expect(obj.update_attributes(:organization => '5')).to eq true
+      expect(obj.organization_id).to eq '5'
+    end
+  end
 end
