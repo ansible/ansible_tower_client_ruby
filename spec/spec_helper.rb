36d8e42180e438a2ecac47027a0d5b09c858f429
Daniel Berger <dberger@redhat.com>
2019-04-19 11:16:19 -0400

    Update bundler, rake, remove unnecessary load path mangling.
    Change factorygirl to factorybot.
    
    Update spec_helper.rb.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,12 @@
 gem_root = Gem::Specification.find_by_name("ansible_tower_client").gem_dir
 Dir[File.join(gem_root, "spec/support/**/*.rb")].each { |f| require f }
 
-require 'factory_girl'
-FactoryGirl.find_definitions
+require 'factory_bot'
+FactoryBot.find_definitions
 
 RSpec.configure do |config|
   config.order = :random
-  config.include FactoryGirl::Syntax::Methods
+  config.include FactoryBot::Syntax::Methods
 end
 
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
